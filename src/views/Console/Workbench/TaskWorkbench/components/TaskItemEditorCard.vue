965eaeeeeb7e1b29c4e1e1095da27d8dc8e1ac8e
wang <w13839575053@qq.com>
2026-03-19 14:22:09 +0800

    完善内联箭头函数

--- src/views/Console/Workbench/TaskWorkbench/components/TaskItemEditorCard.vue
+++ src/views/Console/Workbench/TaskWorkbench/components/TaskItemEditorCard.vue
@@ -59,13 +59,13 @@ const emit = defineEmits<{
         :value="item.platform"
         style="width: 160px"
         :options="PLATFORM_OPTIONS"
-        @update:value="(value) => emit('changePlatform', { item, value: value as TaskFormItem['platform'] })"
+        @update:value="emit('changePlatform', { item, value: $event as TaskFormItem['platform'] })"
       />
       <a-input
         :value="item.title"
         class="task-item-title-input"
         placeholder="请输入题目标题，例如 Two Sum / A+B Problem"
-        @update:value="(value) => emit('changeTitle', { item, value: value as string })"
+        @update:value="emit('changeTitle', { item, value: $event as string })"
       />
     </div>
 
